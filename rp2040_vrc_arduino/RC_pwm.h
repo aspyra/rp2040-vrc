aspyra/rp2040-vrc

commit 410925ed0823c79e4345349c66103774a7aa9add

Restructured most of the code to add 'binding' mode (learning receivers endpoints and midpoint), WIP - breaks a lot of stuff, untested

rp2040_vrc_arduino/RC_pwm.h
@@ -1,11 +1,26 @@
 #include <Arduino.h>
 #include "hardware/timer.h"
+#include <EEPROM.h>
+
+typedef struct{
+  uint16_t pwm_in_max[4];
+  uint16_t pwm_in_min[4];
+  uint16_t pwm_in_mid[4];
+  uint8_t check_byte;
+}eeprom_data_t;
+
+// Global variables and functions -------------------------------------------------------------------
 
 #define CLK_MLTP 12
+uint8_t change_flags = 0;
+uint32_t pwm_in[4];
+eeprom_data_t bindData;
+
+// -------------------------------------------------------------------
 
 volatile uint32_t pulseStartTime[4]; // Support multiple pins if needed
 volatile uint32_t pulseWidth[4];
-volatile uint8_t changeflag = 0;
+volatile uint8_t _change_flags = 0;
 
 void handleRCPWM(int pin) {
   // Read the pin state and decide
@@ -16,7 +31,7 @@ void handleRCPWM(int pin) {
   } else {
     // Falling edge: calculate the pulse width
     pulseWidth[pin] = current_time - pulseStartTime[pin];
-    changeflag |= (1<<pin);
+    _change_flags |= (1<<pin);
   }
 }
 
@@ -25,6 +40,18 @@ void RC_PWM_INT1(){handleRCPWM(1);}
 void RC_PWM_INT2(){handleRCPWM(2);}
 void RC_PWM_INT3(){handleRCPWM(3);}
 
+void update_bindData(){
+  ;
+}
+
+void save_new_bindData(){
+  ;
+}
+
+void discard_new_bindData(){
+  ;
+}
+
 void setup_PWM(){
   for(uint8_t pin = 0; pin < 4; ++pin){
     pinMode(PWM_pins[pin], INPUT_PULLDOWN);
@@ -32,8 +59,38 @@ void setup_PWM(){
   //This magic line increases Watchdog tick 12x effectively speeding up the timer clock 12x giving sub 0.1us accuracy
   *(uint32_t *)(WATCHDOG_BASE + 0x2c) = 0x0000201;
 
+  EEPROM.begin(256);
+  EEPROM.get(0, bindData);
+  if(bindData.check_byte != 0xAA){
+    bindData.check_byte = 0xAA;
+    for(uint8_t ch = 0; ch < 4; ++ch){
+      bindData.pwm_in_max[ch] = 2000 * CLK_MLTP;
+      bindData.pwm_in_min[ch] = 1000 * CLK_MLTP;
+      bindData.pwm_in_mid[ch] = 1500 * CLK_MLTP;
+    }
+    EEPROM.commit();
+  }
+
   attachInterrupt(digitalPinToInterrupt(PWM_pins[0]), RC_PWM_INT0, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PWM_pins[1]), RC_PWM_INT1, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PWM_pins[2]), RC_PWM_INT2, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PWM_pins[3]), RC_PWM_INT3, CHANGE);
+}
+
+bool update_PWM_values(){
+  if(_change_flags){
+    noInterrupts();
+    for(uint8_t pin = 0; pin < 4; ++pin){
+      if((_change_flags>>pin) & 0x01){
+        pwm_in[pin] = pulseWidth[pin];
+        //perform max min on the measured signals
+        pwm_in[pin] = max(bindData.pwm_in_min[pin], min(bindData.pwm_in_max[pin], pwm_in[pin]))-bindData.pwm_in_min[pin];
+      }
+    }
+    change_flags = _change_flags;
+    _change_flags = 0;
+    interrupts();
+    return true;
+  }
+  return false;
 }

rp2040_vrc_arduino/USB_and_HID_report.h
@@ -1,10 +1,19 @@
+#include "Adafruit_TinyUSB.h"
 #include "class\hid\hid_device.h"
+#define MAX_HID_SIGNAL 0xFFFF
 
-typedef struct TU_ATTR_PACKED
-{
+typedef struct TU_ATTR_PACKED{
   uint16_t axis[6];
   uint16_t buttons;
 }hid_custom_gamepad_report_t;
+
+// Global variables and functions -------------------------------------------------------------------
+
+hid_custom_gamepad_report_t gp;
+void loop_gp();
+void send_gp();
+
+// -------------------------------------------------------------------
 
 #define TUD_HID_REPORT_DESC_CUSTOM_GAMEPAD(...)                    \
   HID_USAGE_PAGE ( HID_USAGE_PAGE_DESKTOP     )                   ,\
@@ -18,7 +27,7 @@ typedef struct TU_ATTR_PACKED
     HID_USAGE          ( HID_USAGE_DESKTOP_RY                   ) ,\
     HID_USAGE          ( HID_USAGE_DESKTOP_RZ                   ) ,\
     HID_LOGICAL_MIN_N  ( 0x0000, 2                              ) ,\
-    HID_LOGICAL_MAX_N  ( CLK_MLTP*1000, 2                       ) ,\
+    HID_LOGICAL_MAX_N  ( MAX_HID_SIGNAL, 2                      ) ,\
     HID_REPORT_COUNT   ( 6                                      ) ,\
     HID_REPORT_SIZE    ( 16                                     ) ,\
     HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
@@ -33,4 +42,43 @@ typedef struct TU_ATTR_PACKED
     HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
   HID_COLLECTION_END                                               \
 
-//EOF
+// USB HID object, descriptor & gamepad report (data sent to PC)
+Adafruit_USBD_HID usb_hid;
+uint8_t const desc_hid_report[] = {TUD_HID_REPORT_DESC_CUSTOM_GAMEPAD()};
+
+void setup_gp(){ //TinyUSB stuff
+  // Manual begin() is required on core without built-in support e.g. mbed rp2040
+  if (!TinyUSBDevice.isInitialized()) {
+    TinyUSBDevice.begin(0);
+  }
+
+  // Setup HID
+  usb_hid.setPollInterval(1);
+  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+  usb_hid.begin();
+
+  // If already enumerated, additional class driver begin() e.g msc, hid, midi won't take effect until re-enumeration
+  if (TinyUSBDevice.mounted()) {
+    TinyUSBDevice.detach();
+    delay(10);
+    TinyUSBDevice.attach();
+  }
+}
+
+void loop_gp(){ //TinyUSB stuff
+  #ifdef TINYUSB_NEED_POLLING_TASK
+  // Manual call tud_task since it isn't called by Core's background
+  TinyUSBDevice.task();
+  #endif
+  /*
+  // not enumerated()/mounted() yet: nothing to do
+  if (!TinyUSBDevice.mounted()) {
+    return;
+  }
+
+  if (!usb_hid.ready()) return;*/
+}
+
+void send_gp(){
+  usb_hid.sendReport(0, &gp, sizeof(gp));
+}

rp2040_vrc_arduino/game_specific_settings.h
@@ -1,0 +1,38 @@
+//RP2040 pins for RC PWM input (in order)
+const uint8_t PWM_pins[4] = {0, 1, 2, 3};
+
+//game list
+enum game_t{
+  vrc = 1,
+  circuit_superstars = 2,
+  wrap
+}game = vrc; //Specify default game
+
+//game-dependent input-output translation
+
+void update_gp_by_game(uint8_t _changeflags, hid_custom_gamepad_report_t &_gp, uint32_t *_pwm_in){
+  switch(game){
+    case vrc:
+      for(uint8_t axis = 0; axis < 4; ++axis){
+        _gp.axis[axis] = _pwm_in[axis];
+      }
+      _gp.axis[4] = MAX_HID_SIGNAL/2;
+      _gp.axis[5] = MAX_HID_SIGNAL/2;
+      break;
+
+    case circuit_superstars:
+      _gp.axis[0] = _pwm_in[0];
+      _gp.axis[1] = MAX_HID_SIGNAL/2;
+      if(_pwm_in[1] > MAX_HID_SIGNAL/2){
+        _gp.axis[2] = _pwm_in[1];
+        _gp.axis[3] = MAX_HID_SIGNAL/2;
+      }
+      else{
+        _gp.axis[3] = MAX_HID_SIGNAL-_pwm_in[1];
+        _gp.axis[2] = MAX_HID_SIGNAL/2;    
+      }
+      _gp.axis[4] = MAX_HID_SIGNAL/2;
+      _gp.axis[5] = MAX_HID_SIGNAL/2;
+      break;
+  }
+}

rp2040_vrc_arduino/rp2040_vrc_arduino.ino
@@ -1,197 +1,85 @@
 //MIT license, check LICENSE for more information
 
-#include "settings.h"
-#include "Adafruit_TinyUSB.h"
+#include "USB_and_HID_report.h"
+#include "game_specific_settings.h"
+
 #include "RC_pwm.h"
-#include "hid_report_desc.h"
 #include "status_LED.h"
 #include "boot_button.h"
 
-enum pwm_state {low = 0, mid = 1, high = 2};
-
-// USB HID object, descriptor & gamepad report (data sent to PC)
-Adafruit_USBD_HID usb_hid;
-uint8_t const desc_hid_report[] = {TUD_HID_REPORT_DESC_CUSTOM_GAMEPAD()};
-hid_custom_gamepad_report_t gp;
+enum pwm_state_t {low = 0, mid = 1, high = 2};
+enum adapter_mode_t {running, binding} mode = running;
 
 void setup() {
   setup_LED();
-  LED_set(CRGB::Red);
+  LED_set_constant(CRGB::Red); //mark beginning of setup
+
   Serial.begin(115200);
-  { //TinyUSB stuff
-    // Manual begin() is required on core without built-in support e.g. mbed rp2040
-    if (!TinyUSBDevice.isInitialized()) {
-      TinyUSBDevice.begin(0);
-    }
 
-    // Setup HID
-    usb_hid.setPollInterval(1);
-    usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
-    for(uint8_t axis = 0; axis < 6; ++axis){
-      gp.axis[axis] = CLK_MLTP*500;
-    }
-    usb_hid.begin();
+  setup_gp();
 
-    // If already enumerated, additional class driver begin() e.g msc, hid, midi won't take effect until re-enumeration
-    if (TinyUSBDevice.mounted()) {
-      TinyUSBDevice.detach();
-      delay(10);
-      TinyUSBDevice.attach();
-    }
-  }
   setup_PWM();
-  LED_set(CRGB::Black);
+
+  LED_set_blink(); //mark end of setup
 }
 
 void loop() {
-  { //TinyUSB stuff
-    #ifdef TINYUSB_NEED_POLLING_TASK
-    // Manual call tud_task since it isn't called by Core's background
-    TinyUSBDevice.task();
-    #endif
-
-    // not enumerated()/mounted() yet: nothing to do
-    if (!TinyUSBDevice.mounted()) {
-      return;
+  loop_gp();
+  //check if signal from any channel was received
+  if(update_PWM_values()){
+    if(mode == binding){
+      update_bindData();
     }
-
-    if (!usb_hid.ready()) return;
-  }
-
-  uint32_t pwm_in[4];
-  static unsigned long lastPrintTime = millis(); // To track the last time we printed
-  static uint8_t test_flags = 0;
-
-  { //Wait for PWM signal, copy it and make sure it's in the range
-    //wait until at least one of the channels measures PWM width
-    while (changeflag == 0) {
-      //if no PWM for 1s -> print "No signal"
-      if (millis() - lastPrintTime >= CLK_MLTP*2000) {
-        Serial.println("No signal");
-        lastPrintTime = millis();
-      }
-    }
-    
-    //get the values; no interrupt
-    noInterrupts();
-    for(uint8_t pin = 0; pin < 4; ++pin){
-      pwm_in[pin] = pulseWidth[pin];
-    }
-    test_flags |= changeflag;
-    changeflag = 0;
-    interrupts();
-
-    //perform max min on the measured signals
-    for(uint8_t pin = 0; pin < 4; ++pin){
-      pwm_in[pin] = max(CLK_MLTP*1000, min(CLK_MLTP*2000, pwm_in[pin]))-CLK_MLTP*1000;
-    }
-  }
-
-  switch(game){ //go over the inputs and calculate outputs
-    case vrc:
-      for(uint8_t axis = 0; axis < 4; ++axis){
-        gp.axis[axis] = pwm_in[axis];
-      }
-      break;
-    case circuit_superstars:
-      gp.axis[0] = pwm_in[0];
-      gp.axis[1] = CLK_MLTP*500;
-      if(pwm_in[1] > CLK_MLTP*500){
-        gp.axis[2] = pwm_in[1];
-        gp.axis[3] = CLK_MLTP*500;
-      }
-      else{
-        gp.axis[3] = CLK_MLTP*1000-pwm_in[1];
-        gp.axis[2] = CLK_MLTP*500;    
-      }
-      gp.axis[4] = CLK_MLTP*500;
-      gp.axis[5] = CLK_MLTP*500;
-
-      static pwm_state ch4_state = mid;
-      static uint32_t ch4_timer = 0;
-
-      const uint32_t low_th = CLK_MLTP*250;
-      const uint32_t hi_th = CLK_MLTP*750;
-
-      if(micros() - ch4_timer > CLK_MLTP*50000){
-        gp.buttons = 0;
-        switch(ch4_state){
-          case low:
-            if(pwm_in[3] >= low_th && pwm_in[3] < hi_th){
-              //+ -> menu
-              gp.buttons = (1<<9);
-              ch4_state = mid;
-              ch4_timer = time_us_32();
-            }
-            else if(pwm_in[3] >= hi_th){
-              //- -> back
-              gp.buttons = (1<<1);
-              ch4_state = high;
-              ch4_timer = time_us_32();
-            }
-            break;
-
-          case mid:
-            if(pwm_in[3] >= hi_th){
-              //+ -> menu
-              gp.buttons = (1<<9);
-              ch4_state = high;
-              ch4_timer = time_us_32();
-            }
-            else if(pwm_in[3] < low_th){
-              //- -> back
-              gp.buttons = (1<<1);
-              ch4_state = low;
-              ch4_timer = time_us_32();
-            }
-            break;
-
-          case high:
-            if(pwm_in[3] < low_th){
-              //+ -> menu
-              gp.buttons = (1<<9);
-              ch4_state = low;
-              ch4_timer = time_us_32();
-            }
-            else if(pwm_in[3] >= low_th && pwm_in[3] < hi_th){
-              //- -> back
-              gp.buttons = (1<<1);
-              ch4_state = mid;
-              ch4_timer = time_us_32();
-            }
-            break;
-        }
-      }
-
-      if(pwm_in[2] > 500){ //CH3 controls button 4 (reset position + optionally opponent names)
-        gp.buttons |= (1<<4);
-      }
-      else{
-        gp.buttons &= ~(1<<4);
-      }
-      break;
-  }
-
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-
-  bool buttonState = get_bootsel_button();
-  static bool prevButton = false;
-  if(!prevButton && buttonState){
-    game = static_cast<gm>((static_cast<int>(game) + 1));
-    if(game == wrap) game = static_cast<gm>(1);
-    LED_update(true);
-    LED_set(CRGB::Red);
+    update_gp_by_game(change_flags, gp, pwm_in);
+    send_gp();
   }
   else{
-    LED_update();
-  }
-  prevButton = buttonState;
-  
+    //if no input signal, do all the stuff that isn't critical
 
-
-  if (millis() - lastPrintTime >= CLK_MLTP*500) { //debug printing
-    Serial.printf("CH1 %i CH2 %i CH3 %i CH4 %i, flags 0x%x b%i\n", pwm_in[0], pwm_in[1], pwm_in[2], pwm_in[3], test_flags, buttonState);
-    test_flags = 0;
-    lastPrintTime = millis(); // Update the last print time
+    static bool long_press = false;
+    switch(updateButtonState()){
+      case LONG_PRESSED:
+        long_press = true;
+        if(mode == running){
+          LED_set_constant(CRGB::Blue);
+        }else if(mode == binding){
+          LED_set_constant(CRGB::Red);
+        }
+        break;
+      case SHORT_PRESSED:
+        long_press = false;
+        if(mode == running){
+          LED_set_constant(CRGB::Yellow);
+        }else if(mode == binding){
+          LED_set_constant(CRGB::Yellow);
+        }
+        break;
+      case NOT_PRESSED:
+        if(mode == running){
+          if(long_press){
+            mode = binding;
+            LED_set_blink();
+          }
+          else{ //short press
+            game = static_cast<game_t>(game + 1);
+            if (game >= wrap) {
+              game = static_cast<game_t>(0);
+            }
+          }
+        }else if(mode == binding){
+          mode = running;
+          if(!long_press){
+            save_new_bindData();
+          }else{
+            discard_new_bindData();
+          }
+          LED_set_blink();
+        }
+      case NOT_CHANGED:
+        break;
+    }
+    loop_LED(false);
+    //Serial.printf("CH1 %i CH2 %i CH3 %i CH4 %i\n", pwm_in[0], pwm_in[1], pwm_in[2], pwm_in[3]);
   }
 }
+

commit 5b0279b5fca9b5b98afb9cca629efe2650ba411f

Fixing stuff broken in commit 410925e, compiles - to be tested

rp2040_vrc_arduino/RC_pwm.h
@@ -2,6 +2,11 @@
 #include "hardware/timer.h"
 #include <EEPROM.h>
 
+//RP2040 pins for RC PWM input (in order)
+const uint8_t PWM_pins[4] = {0, 1, 2, 3};
+
+const uint32_t CLK_MLTP = 12; //we trick the timer into running 12x faster, so we need to scale ALL time references
+
 typedef struct{
   uint16_t pwm_in_max[4];
   uint16_t pwm_in_min[4];
@@ -9,12 +14,15 @@ typedef struct{
   uint8_t check_byte;
 }eeprom_data_t;
 
+const uint32_t pwm_in_abs_min = 98 * CLK_MLTP;
+const uint32_t pwm_in_abs_max = 2550 * CLK_MLTP;
+const uint32_t bind_offset = 2 * CLK_MLTP; //1ms of min/max offset that ensures 'saturation' on ends
+
 // Global variables and functions -------------------------------------------------------------------
 
-#define CLK_MLTP 12
 uint8_t change_flags = 0;
 uint32_t pwm_in[4];
-eeprom_data_t bindData;
+eeprom_data_t bindData, newBindData;
 
 // -------------------------------------------------------------------
 
@@ -41,15 +49,44 @@ void RC_PWM_INT2(){handleRCPWM(2);}
 void RC_PWM_INT3(){handleRCPWM(3);}
 
 void update_bindData(){
-  ;
+  for(uint8_t ch = 0; ch < 4; ++ch){
+    if((pwm_in[ch] >= pwm_in_abs_min) && (pwm_in[ch] <= pwm_in_abs_max)){ //make sure signal makes sense
+      if(pwm_in[ch] > newBindData.pwm_in_max[ch]){
+        newBindData.pwm_in_max[ch] = pwm_in[ch] - bind_offset;
+      }
+      if(pwm_in[ch] < newBindData.pwm_in_min[ch]){
+        newBindData.pwm_in_min[ch] = pwm_in[ch] + bind_offset;
+      }
+      newBindData.pwm_in_mid[ch] = pwm_in[ch];
+    }
+  }
+}
+
+void debug_bindData(){
+  static unsigned long lastDebugTime = 0;
+  if((millis() - lastDebugTime) > (500 * CLK_MLTP)){
+    Serial.printf("Inputs: CH1 %fus, CH2 %fus, CH3 %fus, CH4 %fus\n", float(pwm_in[0])/12.0, float(pwm_in[1])/12.0, float(pwm_in[2])/12.0, float(pwm_in[3])/12.0);
+    for(uint8_t ch = 0; ch < 4; ++ch){
+      Serial.printf("Previous bind setting for CH%i: min &fus, mid %fus, max %fus  ", ch, float(bindData.pwm_in_min[ch])/12.0, float(bindData.pwm_in_min[ch])/12.0, float(bindData.pwm_in_min[ch])/12.0);
+      Serial.printf("New bind setting for CH%i: min &fus, mid %fus, max %fus\n", ch, float(newBindData.pwm_in_min[ch])/12.0, float(newBindData.pwm_in_min[ch])/12.0, float(newBindData.pwm_in_min[ch])/12.0);
+    }
+  }
+}
+
+void reset_new_bindData(){
+  newBindData.check_byte = 0xAA;
+  for(uint8_t ch = 0; ch < 4; ++ch){
+      bindData.pwm_in_max[ch] = pwm_in_abs_min;
+      bindData.pwm_in_min[ch] = pwm_in_abs_max;
+      bindData.pwm_in_mid[ch] = 0;    
+  }
 }
 
 void save_new_bindData(){
-  ;
-}
-
-void discard_new_bindData(){
-  ;
+  memcpy(&bindData, &newBindData, sizeof(eeprom_data_t));
+  reset_new_bindData();
+  EEPROM.put(0, bindData);
+  EEPROM.commit();
 }
 
 void setup_PWM(){
@@ -61,15 +98,17 @@ void setup_PWM(){
 
   EEPROM.begin(256);
   EEPROM.get(0, bindData);
-  if(bindData.check_byte != 0xAA){
+  if(bindData.check_byte != 0xAA){//if check_byte is not correct, restore 'default' values
     bindData.check_byte = 0xAA;
     for(uint8_t ch = 0; ch < 4; ++ch){
       bindData.pwm_in_max[ch] = 2000 * CLK_MLTP;
       bindData.pwm_in_min[ch] = 1000 * CLK_MLTP;
       bindData.pwm_in_mid[ch] = 1500 * CLK_MLTP;
     }
+    EEPROM.put(0, bindData);
     EEPROM.commit();
   }
+  reset_new_bindData();
 
   attachInterrupt(digitalPinToInterrupt(PWM_pins[0]), RC_PWM_INT0, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PWM_pins[1]), RC_PWM_INT1, CHANGE);
@@ -83,14 +122,33 @@ bool update_PWM_values(){
     for(uint8_t pin = 0; pin < 4; ++pin){
       if((_change_flags>>pin) & 0x01){
         pwm_in[pin] = pulseWidth[pin];
-        //perform max min on the measured signals
-        pwm_in[pin] = max(bindData.pwm_in_min[pin], min(bindData.pwm_in_max[pin], pwm_in[pin]))-bindData.pwm_in_min[pin];
       }
     }
     change_flags = _change_flags;
     _change_flags = 0;
     interrupts();
+
+    for(uint8_t pin = 0; pin < 4; ++pin){
+      if((change_flags>>pin) & 0x01){
+        	//perform max min on the measured signals
+	        pwm_in[pin] = max(bindData.pwm_in_min[pin], min(bindData.pwm_in_max[pin], pwm_in[pin]))-bindData.pwm_in_min[pin];
+      }
+    }
+
     return true;
   }
   return false;
+}
+
+uint32_t scale_pwm(uint8_t channel, uint32_t out_min, uint32_t out_max){ //extended map() function to include mid-point. Beware of uint32_t overflows
+  if(pwm_in[channel] >= bindData.pwm_in_mid[channel]){
+    uint32_t numerator = (pwm_in[channel] - bindData.pwm_in_mid[channel])*(out_max - out_min);
+    uint32_t denominator = (bindData.pwm_in_max[channel] - bindData.pwm_in_mid[channel]);
+    return ((numerator / denominator) + (out_max + out_min))/2;
+  }
+  else{
+    uint32_t numerator = (pwm_in[channel] - bindData.pwm_in_min[channel])*(out_max - out_min);
+    uint32_t denominator = (bindData.pwm_in_mid[channel] - bindData.pwm_in_min[channel]);
+    return (numerator / denominator)/2 + out_min;
+  }
 }

rp2040_vrc_arduino/USB_and_HID_report.h
@@ -1,6 +1,8 @@
 #include "Adafruit_TinyUSB.h"
 #include "class\hid\hid_device.h"
 #define MAX_HID_SIGNAL 0xFFFF
+#define MIN_HID_SIGNAL 0x0000
+#define MID_HID_SIGNAL (MAX_HID_SIGNAL + MIN_HID_SIGNAL)/2
 
 typedef struct TU_ATTR_PACKED{
   uint16_t axis[6];
@@ -26,7 +28,7 @@ void send_gp();
     HID_USAGE          ( HID_USAGE_DESKTOP_RX                   ) ,\
     HID_USAGE          ( HID_USAGE_DESKTOP_RY                   ) ,\
     HID_USAGE          ( HID_USAGE_DESKTOP_RZ                   ) ,\
-    HID_LOGICAL_MIN_N  ( 0x0000, 2                              ) ,\
+    HID_LOGICAL_MIN_N  ( MIN_HID_SIGNAL, 2                      ) ,\
     HID_LOGICAL_MAX_N  ( MAX_HID_SIGNAL, 2                      ) ,\
     HID_REPORT_COUNT   ( 6                                      ) ,\
     HID_REPORT_SIZE    ( 16                                     ) ,\

rp2040_vrc_arduino/game_specific_settings.h
@@ -1,6 +1,3 @@
-//RP2040 pins for RC PWM input (in order)
-const uint8_t PWM_pins[4] = {0, 1, 2, 3};
-
 //game list
 enum game_t{
   vrc = 1,
@@ -9,30 +6,31 @@ enum game_t{
 }game = vrc; //Specify default game
 
 //game-dependent input-output translation
-
 void update_gp_by_game(uint8_t _changeflags, hid_custom_gamepad_report_t &_gp, uint32_t *_pwm_in){
   switch(game){
     case vrc:
       for(uint8_t axis = 0; axis < 4; ++axis){
-        _gp.axis[axis] = _pwm_in[axis];
+		    if(_changeflags>>axis & 0x01){
+		      _gp.axis[axis] = scale_pwm(_pwm_in[axis], MIN_HID_SIGNAL, MAX_HID_SIGNAL);
+		    }
       }
-      _gp.axis[4] = MAX_HID_SIGNAL/2;
-      _gp.axis[5] = MAX_HID_SIGNAL/2;
+      _gp.axis[4] = MID_HID_SIGNAL;
+      _gp.axis[5] = MID_HID_SIGNAL;
       break;
 
     case circuit_superstars:
       _gp.axis[0] = _pwm_in[0];
-      _gp.axis[1] = MAX_HID_SIGNAL/2;
-      if(_pwm_in[1] > MAX_HID_SIGNAL/2){
+      _gp.axis[1] = MID_HID_SIGNAL;
+      if(_pwm_in[1] > MID_HID_SIGNAL){
         _gp.axis[2] = _pwm_in[1];
-        _gp.axis[3] = MAX_HID_SIGNAL/2;
+        _gp.axis[3] = MID_HID_SIGNAL;
       }
       else{
         _gp.axis[3] = MAX_HID_SIGNAL-_pwm_in[1];
-        _gp.axis[2] = MAX_HID_SIGNAL/2;    
+        _gp.axis[2] = MID_HID_SIGNAL;   
       }
-      _gp.axis[4] = MAX_HID_SIGNAL/2;
-      _gp.axis[5] = MAX_HID_SIGNAL/2;
+      _gp.axis[4] = MID_HID_SIGNAL;
+      _gp.axis[5] = MID_HID_SIGNAL;
       break;
   }
 }

rp2040_vrc_arduino/rp2040_vrc_arduino.ino
@@ -1,11 +1,11 @@
 //MIT license, check LICENSE for more information
 
 #include "USB_and_HID_report.h"
-#include "game_specific_settings.h"
-
 #include "RC_pwm.h"
 #include "status_LED.h"
 #include "boot_button.h"
+
+#include "game_specific_settings.h" //requires "RC_pwm.h"
 
 enum pwm_state_t {low = 0, mid = 1, high = 2};
 enum adapter_mode_t {running, binding} mode = running;
@@ -35,51 +35,72 @@ void loop() {
   }
   else{
     //if no input signal, do all the stuff that isn't critical
+    process_button();
+    if(mode == binding){
+      debug_bindData();
+      CRGB colors[4] = {CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue};
+      for(uint8_t ch = 0; ch < 4; ++ch){
+        if(pwm_in[ch] <= bindData.pwm_in_min[ch]){
+          colors[ch] += CRGB::Green;
+        }
+        if(pwm_in[ch] >= bindData.pwm_in_max[ch]){
+          colors[ch] += CRGB::Red;
+        }
+        if((pwm_in[ch] >= (bindData.pwm_in_mid[ch] - bind_offset)) && (pwm_in[ch] <= (bindData.pwm_in_mid[ch] + bind_offset))){
+          colors[ch] = CRGB::White;
+        }
+      }
+       loop_LED(4, colors, 4);
+    }
+    else{
+      CRGB colors[1] = {CRGB::Green};
+      loop_LED(game, colors, 1);
+    }
+  }
+}
 
-    static bool long_press = false;
-    switch(updateButtonState()){
-      case LONG_PRESSED:
-        long_press = true;
-        if(mode == running){
-          LED_set_constant(CRGB::Blue);
-        }else if(mode == binding){
-          LED_set_constant(CRGB::Red);
-        }
-        break;
-      case SHORT_PRESSED:
-        long_press = false;
-        if(mode == running){
-          LED_set_constant(CRGB::Yellow);
-        }else if(mode == binding){
-          LED_set_constant(CRGB::Yellow);
-        }
-        break;
-      case NOT_PRESSED:
-        if(mode == running){
-          if(long_press){
-            mode = binding;
-            LED_set_blink();
-          }
-          else{ //short press
-            game = static_cast<game_t>(game + 1);
-            if (game >= wrap) {
-              game = static_cast<game_t>(0);
-            }
-          }
-        }else if(mode == binding){
-          mode = running;
-          if(!long_press){
-            save_new_bindData();
-          }else{
-            discard_new_bindData();
-          }
+void process_button(){
+  static bool long_press = false;
+  switch(updateButtonState()){
+    case LONG_PRESSED:
+      long_press = true;
+      if(mode == running){
+        LED_set_constant(CRGB::Blue);
+      }else if(mode == binding){
+        LED_set_constant(CRGB::Red);
+      }
+      break;
+    case SHORT_PRESSED:
+      long_press = false;
+      if(mode == running){
+        LED_set_constant(CRGB::Yellow);
+      }else if(mode == binding){
+        LED_set_constant(CRGB::Yellow);
+      }
+      break;
+    case NOT_PRESSED:
+      if(mode == running){
+        if(long_press){
+          mode = binding;
           LED_set_blink();
         }
-      case NOT_CHANGED:
-        break;
-    }
-    loop_LED(false);
-    //Serial.printf("CH1 %i CH2 %i CH3 %i CH4 %i\n", pwm_in[0], pwm_in[1], pwm_in[2], pwm_in[3]);
+        else{ //short press
+          game = static_cast<game_t>(game + 1);
+          if (game >= wrap) {
+            game = static_cast<game_t>(0);
+          }
+        }
+      }else if(mode == binding){
+        mode = running;
+        if(long_press){
+          save_new_bindData();
+        }else{
+          reset_new_bindData();
+        }
+        LED_set_blink();
+      }
+    case NOT_CHANGED:
+      break;
   }
 }
 

rp2040_vrc_arduino/status_LED.h
@@ -10,6 +10,7 @@ CRGB blinkColor = CRGB::Green;
 // -------------------------------------------------------------------
 
 bool blinkMode = false;
+bool last_blinkMode = false;
 const uint8_t RGB_pin = 16;
 const unsigned long blinkInterval = 250*CLK_MLTP; // 250 ms on/off
 const unsigned long gapInterval = 1000*CLK_MLTP; // 1 second gap
@@ -23,6 +24,7 @@ void LED_set(CRGB color){
 
 void LED_set_constant(CRGB color){
   blinkMode = false;
+  last_blinkMode = false;
   LED_set(color);
 }
 
@@ -31,66 +33,51 @@ void LED_set_blink(){
   LED_set(CRGB::Black);
 }
 
-void LED_set_blink(CRGB color){
-  blinkMode = true;
-  LED_set(CRGB::Black);
-  blinkColor = color;
-}
-
 void setup_LED(){
   FastLED.setBrightness(RGB_brightness);
   FastLED.addLeds<WS2812, RGB_pin, GRB>(&RGB_LED, 1);  // GRB ordering is typical
 }
 
-void loop_LED(uint8_t blink_count){
-  static unsigned long previousMillis = 0;  // Tracks time for LED blinking
-  static int blinkCount = 0; // Counts completed on/off cycles
-  static bool ledState = false; // Tracks the current state of the LED
-  static unsigned long gapStartMillis = 0; // Tracks time during the gap
-  static bool last_blinkMode = false;
-
-  if(blinkMode){
-    if(!last_blinkMode){
-      //blink mode was just activated - reset all statics
-      blinkCount = blink_count; // Reset for the next cycle
-      gapStartMillis = millis(); // Reset gap timer
-    }
-
-
-
-
-  }
-
-
-  if (!reset_blink && (blinkCount < game)) {
-    unsigned long currentMillis = millis();
-
-    if (currentMillis - previousMillis >= blinkInterval) {
-      previousMillis = currentMillis; // Update the last time LED was toggled
-      ledState = !ledState; // Toggle LED state
-      if(ledState)  LED_set_constant(CRGB::Green);
-      else          LED_set_constant(CRGB::Black);
-
-      if (!ledState) { // Count only complete on/off cycles
-          blinkCount++;
+void loop_LED(uint8_t target_blink_count, CRGB *colors, uint8_t color_count) {  
+  static unsigned long previousMillis = 0;  
+  static uint8_t blinkCount = 0;  
+  static bool ledState = false;  
+  static unsigned long gapStartMillis = 0;  
+  unsigned long currentMillis = millis();  
+  
+  // Check if blinkMode just got activated
+  if (blinkMode) {
+    if (!last_blinkMode) {  
+      previousMillis = currentMillis; // Reset timing  
+      blinkCount = 0; // Reset blink counter  
+      ledState = false; // Ensure LED starts off  
+      gapStartMillis = 0; // Reset gap timer
+      last_blinkMode = true;
+    }  
+    if (blinkCount < target_blink_count) {  
+      if (currentMillis - previousMillis >= blinkInterval) {  
+        previousMillis = currentMillis; // Update the last time LED was toggled  
+        ledState = !ledState; // Toggle LED state  
+  
+        if (ledState) {  
+          uint8_t colorIndex = blinkCount % color_count; // Wrap color index  
+          LED_set(colors[colorIndex]);  
+        } else {  
+          LED_set(CRGB::Black);  
+          blinkCount++;  
+        }  
+      }  
+    } else {  
+      // Start the 1-second gap if blinking is complete  
+      if (gapStartMillis == 0) {  
+        gapStartMillis = currentMillis;  
+        LED_set(CRGB::Black); // Turn off LED  
+      }  
+  
+      if (currentMillis - gapStartMillis >= gapInterval) {  
+        blinkMode = false; // Optionally, stop blinking after one sequence  
+        gapStartMillis = 0; // Reset gap timer for the next round  
       }
-    }
-  } else {
-    if(reset_blink){
-      blinkCount = game; // Reset for the next cycle
-      gapStartMillis = 0; // Reset gap timer      
-    }
-    // Start the 1-second gap if blinking is complete
-    unsigned long currentMillis = millis();
-
-    if (gapStartMillis == 0) {
-        gapStartMillis = currentMillis;
-        LED_set_constant(CRGB::Black);
-    }
-
-    if (currentMillis - gapStartMillis >= gapInterval) {
-        blinkCount = 0; // Reset for the next cycle
-        gapStartMillis = 0; // Reset gap timer
     }
   }
 }